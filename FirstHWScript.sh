c5011a3726b0c9772cfb93de98134bf5fae607b2
RunFMe <leleytner@gmail.com>
2019-02-19 22:36:55 +0500

    Add pruning

--- FirstHWScript.sh
+++ FirstHWScript.sh
@@ -6,17 +6,17 @@ shift
 archive_name=$1
 shift
 
+path_to_dir=$(realpath $to_dir)
 # copy files into specified dir
 mkdir -p $to_dir
-files=('')
 while [ $# -gt 0 ]; do
-    find ~ -name "*.$1" -exec cp "{}" $to_dir --force --backup=numbered \; 2> /dev/null
+    find ~ -path "$path_to_dir" -prune -o -name "*.$1" -exec cp "{}" $to_dir --force --backup=numbered \; 2> /dev/null
     shift
 done
 
 # compress dir into archive
 path_to_archive=$(realpath $archive_name)
 cd $to_dir
-tar -czf $path_to_archive * 2> /dev/null
+tar -czf "$path_to_archive" * 2> /dev/null
 
 echo "done"
