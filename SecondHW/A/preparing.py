5dbd291a816f0f2235fec54e32737ef8f9924e09
RunFMe <leleytner@gmail.com>
2019-03-03 18:56:18 +0500

    Second HW done

---
+++ SecondHW/A/preparing.py
@@ -0,0 +1,6 @@
+with open('index.h', 'w+') as f:
+    f.write('''// nothing to see here
+            // empty header
+            // it was not required to fill it
+            // so it is empty
+            // that's it''')
